39ef38770a131f1c76fd79e6a892b8d081d3ae37
Jerome Simeon <jeromesimeon@me.com>
2019-03-06 13:13:30 -0500

    (fix) Add a test with DateTime having a timezone offset
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- test/serializer/jsongenerator.js
+++ test/serializer/jsongenerator.js
@@ -148,6 +148,10 @@ describe('JSONGenerator', () => {
             let date = Moment.parseZone('Wed, 09 Aug 1995 00:00:00 GMT');
             jsonGenerator.convertToJSON({ getType: () => { return 'DateTime'; } }, date).should.equal('1995-08-09T00:00:00Z');
         });
+        it('should convert a date time object to ISOString in a different timezone', () => {
+            let date = Moment.parseZone('Wed, 09 Aug 1995 00:00:00 -0500');
+            jsonGenerator.convertToJSON({ getType: () => { return 'DateTime'; } }, date).should.equal('1995-08-09T00:00:00-05:00');
+        });
 
         it('should pass through a boolean object', () => {
             jsonGenerator.convertToJSON({ getType: () => { return 'Boolean'; } }, true).should.equal(true);
