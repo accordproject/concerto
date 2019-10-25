5780ade6255e0858782a870bc7e36031941b3e59
Sagnik Mandal <sonalisagnik123@gmail.com>
2019-10-25 18:38:01 +0530

    Update javavisitor.js

--- packages/concerto-tools/lib/codegen/fromcto/java/javavisitor.js
+++ packages/concerto-tools/lib/codegen/fromcto/java/javavisitor.js
@@ -362,7 +362,7 @@ public abstract class Resource
     toJavaType(type) {
         switch(type) {
         case 'DateTime':
-            return 'java.util.Date';
+            return 'java.util.Instant';
         case 'Boolean':
             return 'boolean';
         case 'String':
