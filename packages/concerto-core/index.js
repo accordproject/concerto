d9a2416009ec98abc07bac2be0e10b13bd774b6c
Dan Selman <danscode@selman.org>
2020-04-09 14:41:26 +0100

    (feat) make Concerto a class
    Signed-off-by: Dan Selman <danscode@selman.org>

--- packages/concerto-core/index.js
+++ packages/concerto-core/index.js
@@ -52,19 +52,4 @@ module.exports.TransactionDeclaration = require('./lib/introspect/transactiondec
 module.exports.Typed = require('./lib/model/typed');
 module.exports.ModelUtil = require('./lib/modelutil');
 module.exports.version = require('./package.json');
-
-module.exports.Concerto = {
-    getTypeDeclaration : require('./lib/concerto').getTypeDeclaration,
-    getIdentifier : require('./lib/concerto').getIdentifier,
-    setIdentifier : require('./lib/concerto').setIdentifier,
-    getFullyQualifiedIdentifier : require('./lib/concerto').getFullyQualifiedIdentifier,
-    toURI : require('./lib/concerto').toURI,
-    fromURI : require('./lib/concerto').fromURI,
-    getType : require('./lib/concerto').getType,
-    getNamespace : require('./lib/concerto').getNamespace,
-    instanceOf : require('./lib/concerto').instanceOf,
-    isIdentifiable : require('./lib/concerto').isIdentifiable,
-    isRelationship : require('./lib/concerto').isRelationship,
-    isObject : require('./lib/concerto').isObject,
-    validate : require('./lib/validator').validate,
-};
+module.exports.Concerto = require('./lib/concerto');
