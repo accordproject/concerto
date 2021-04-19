c04cf1c7ef4d57502d18b5aa3920b03e5a49eca1
Matt Roberts <code@rbrts.uk>
2021-04-19 11:18:01 +0100

    feat(build): Add babel
    Signed-off-by: Matt Roberts <code@rbrts.uk>

--- packages/concerto-core/rollup.config.js
+++ packages/concerto-core/rollup.config.js
@@ -1,24 +1,25 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
-import { terser } from "rollup-plugin-terser";
+import { babel } from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 export default [
-	// browser-friendly UMD build
-	{
-		input: 'index.js',
-		output: {
-			name: 'concerto',
-			file: pkg.browser,
-			format: 'iife',
-      exports: 'named'
-		},
-		plugins: [
-			resolve(),
-			commonjs(),
-      json(),
-      terser(),
-		]
-	},
+    {
+        input: 'index.js',
+        output: {
+            name: 'concerto',
+            file: pkg.browser,
+            format: 'umd',
+            exports: 'named'
+        },
+        plugins: [
+            resolve(),
+            json(),
+            commonjs(),
+            babel({ babelHelpers: 'bundled' }),
+            terser(),
+        ]
+    },
 ];
