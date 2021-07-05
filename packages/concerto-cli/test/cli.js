dbed7f52055c02a2b8879c891b1073668a0044c0
Dan Selman <danscode@selman.org>
2021-07-05 13:34:06 +0100

    test(graphql) : add CLI test
    Signed-off-by: Dan Selman <danscode@selman.org>

--- packages/concerto-cli/test/cli.js
+++ packages/concerto-cli/test/cli.js
@@ -159,6 +159,12 @@ describe('cicero-cli', () => {
             fs.readdirSync(dir.path).length.should.be.above(0);
             dir.cleanup();
         });
+        it('should compile to a GraphQL model', async () => {
+            const dir = await tmp.dir({ unsafeCleanup: true});
+            await Commands.compile('GraphQL', models, dir.path, {offline:false});
+            fs.readdirSync(dir.path).length.should.be.above(0);
+            dir.cleanup();
+        });
         it('should not compile to an unknown model', async () => {
             const dir = await tmp.dir({ unsafeCleanup: true});
             await Commands.compile('BLAH', models, dir.path, {offline:false});
