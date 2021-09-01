44932532846835825551b2eb7b0a1804c4ad3579
Dan Selman <danscode@selman.org>
2021-09-01 16:22:50 +0200

    test(csharp) : add a cli test for csharp target
    Signed-off-by: Dan Selman <danscode@selman.org>

--- packages/concerto-cli/test/cli.js
+++ packages/concerto-cli/test/cli.js
@@ -165,6 +165,12 @@ describe('cicero-cli', () => {
             fs.readdirSync(dir.path).length.should.be.above(0);
             dir.cleanup();
         });
+        it('should compile to a CSharp model', async () => {
+            const dir = await tmp.dir({ unsafeCleanup: true });
+            await Commands.compile('CSharp', models, dir.path, {offline:false});
+            fs.readdirSync(dir.path).length.should.be.above(0);
+            dir.cleanup();
+        });
         it('should not compile to an unknown model', async () => {
             const dir = await tmp.dir({ unsafeCleanup: true });
             await Commands.compile('BLAH', models, dir.path, {offline:false});
