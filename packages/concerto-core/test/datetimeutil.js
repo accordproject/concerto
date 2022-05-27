c1dd2294e1ac3fdda15536e995cf799d71d5b4f0
Ayman <76606666+Ayman161803@users.noreply.github.com>
2022-05-27 16:00:12 +0530

    fix(concerto-core): Improve error logs in datetimeutil.js (#430)
    Signed-off-by: Ayman161803 <mohammedaymannawaz@gmail.com>

--- packages/concerto-core/test/datetimeutil.js
+++ packages/concerto-core/test/datetimeutil.js
@@ -32,7 +32,10 @@ describe('Initialize current time', () => {
         return currentTime.format().should.equal('1970-01-01T00:00:00+05:00');
     });
     it('Should fail for a non-well-formed date/time', function () {
-        return (() => DateTimeUtil.setCurrentTime('foobar')).should.throw('Cannot set current time to \'foobar\' with UTC offset \'undefined\'');
+        return (() => DateTimeUtil.setCurrentTime('foobar')).should.throw('Current time \'foobar\' is not in standard UTC format');
+    });
+    it('Should fail for an invalid UTC offset', function () {
+        return (() => DateTimeUtil.setCurrentTime('1970-01-01T00:00:00+05:00',999999999999)).should.throw('Cannot set current time to \'1970-01-01T00:00:00+05:00\' with UTC offset \'999999999999\'');
     });
     it('Should not fail when currentTime is null', function () {
         const { currentTime } = DateTimeUtil.setCurrentTime(null);
