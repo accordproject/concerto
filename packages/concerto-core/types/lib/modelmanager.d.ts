1f58582bfcb80c142d1b97dde22734a98bba9f39
Matt Roberts <Matt.Roberts@docusign.com>
2022-08-16 12:55:17 +0100

    chore(core): update API
    Signed-off-by: Matt Roberts <Matt.Roberts@docusign.com>

--- packages/concerto-core/types/lib/modelmanager.d.ts
+++ packages/concerto-core/types/lib/modelmanager.d.ts
@@ -18,9 +18,11 @@ declare class ModelManager extends BaseModelManager {
      * @constructor
      * @param {object} [options] - ModelManager options, also passed to Serializer
      * @param {boolean} [options.versionedNamespacesStrict] - require versioned namespaces and imports
+     * @param {Object} [options.regExp] - An alternative regular expression engine.
      */
     constructor(options?: {
         versionedNamespacesStrict?: boolean;
+        regExp?: any;
     });
     /**
      * Adds a model in CTO format to the ModelManager.
