43b790a347b125a627d3f9b2ac6933d1277351c0
Ertugrul Karademir <ekarademir@gmail.com>
2023-10-05 10:22:06 +0100

    Add a builder for the config
    Signed-off-by: Ertugrul Karademir <ekarademir@gmail.com>

--- packages/concerto-analysis/src/compare-config.ts
+++ packages/concerto-analysis/src/compare-config.ts
@@ -16,55 +16,133 @@ import { ComparerFactory } from './comparer';
 import { comparerFactories } from './comparers';
 
 export enum CompareResult {
-    NONE,
-    PATCH,
-    MINOR,
-    MAJOR,
-    ERROR,
+  NONE,
+  PATCH,
+  MINOR,
+  MAJOR,
+  ERROR,
 }
 
 export function compareResultToString(result: CompareResult) {
-    switch (result) {
+  switch (result) {
     case CompareResult.NONE:
-        return 'none';
+      return 'none';
     case CompareResult.PATCH:
-        return 'patch';
+      return 'patch';
     case CompareResult.MINOR:
-        return 'minor';
+      return 'minor';
     case CompareResult.MAJOR:
-        return 'major';
+      return 'major';
     case CompareResult.ERROR:
-        return 'error';
-    }
+      return 'error';
+  }
 }
 
 export type CompareConfig = {
-    comparerFactories: ComparerFactory[];
-    rules: Record<string, CompareResult>;
-}
+  comparerFactories: ComparerFactory[];
+  rules: Record<string, CompareResult>;
+};
 
 export const defaultCompareConfig: CompareConfig = {
-    comparerFactories,
-    rules: {
-        'class-declaration-added': CompareResult.MINOR,
-        'class-declaration-removed': CompareResult.MAJOR,
-        'class-declaration-type-changed': CompareResult.MAJOR,
-        'required-property-added': CompareResult.MAJOR,
-        'optional-property-added': CompareResult.PATCH,
-        'required-property-removed': CompareResult.MAJOR,
-        'optional-property-removed': CompareResult.MAJOR,
-        'namespace-changed': CompareResult.ERROR,
-        'enum-value-added': CompareResult.PATCH,
-        'enum-value-removed': CompareResult.MAJOR,
-        'property-type-changed': CompareResult.MAJOR,
-        'property-validator-added': CompareResult.MAJOR,
-        'property-validator-removed': CompareResult.PATCH,
-        'property-validator-changed': CompareResult.MAJOR,
-        'map-key-type-changed': CompareResult.MAJOR,
-        'map-value-type-changed': CompareResult.MAJOR,
-        'scalar-extends-changed': CompareResult.MAJOR,
-        'scalar-validator-added' : CompareResult.MAJOR,
-        'scalar-validator-removed' : CompareResult.PATCH,
-        'scalar-validator-changed' : CompareResult.MAJOR,
-    }
+  comparerFactories,
+  rules: {
+    'class-declaration-added': CompareResult.MINOR,
+    'class-declaration-removed': CompareResult.MAJOR,
+    'class-declaration-type-changed': CompareResult.MAJOR,
+    'required-property-added': CompareResult.MAJOR,
+    'optional-property-added': CompareResult.PATCH,
+    'required-property-removed': CompareResult.MAJOR,
+    'optional-property-removed': CompareResult.MAJOR,
+    'namespace-changed': CompareResult.ERROR,
+    'enum-value-added': CompareResult.PATCH,
+    'enum-value-removed': CompareResult.MAJOR,
+    'property-type-changed': CompareResult.MAJOR,
+    'property-validator-added': CompareResult.MAJOR,
+    'property-validator-removed': CompareResult.PATCH,
+    'property-validator-changed': CompareResult.MAJOR,
+    'map-key-type-changed': CompareResult.MAJOR,
+    'map-value-type-changed': CompareResult.MAJOR,
+    'scalar-extends-changed': CompareResult.MAJOR,
+    'scalar-validator-added': CompareResult.MAJOR,
+    'scalar-validator-removed': CompareResult.PATCH,
+    'scalar-validator-changed': CompareResult.MAJOR,
+  },
 };
+
+const EmptyConfig: CompareConfig = {
+  comparerFactories: [],
+  rules: {},
+};
+
+export class CompareConfigBuilder {
+  /**
+   * A utility to build CompareConfig to be used in Compare class.
+   * A new compare config can be edited with provided functions and finally
+   * resulting config can be used by calling `build`.
+   *
+   * By default, it starts with an emoty configuration.
+   */
+
+  private _config: CompareConfig = EmptyConfig;
+
+  /**
+   * Final step of the builder
+   *
+   * @returns Resulting CompareConfig object.
+   */
+  public build(): CompareConfig {
+    return { ...this._config };
+  }
+
+  /**
+   * Sets default comparer configuration.
+   */
+  public default() {
+    this._config = { ...defaultCompareConfig };
+  }
+
+  /**
+   * Extends existing configuration tha't built up to this point
+   * with the provided config.
+   *
+   * @param config
+   */
+  public extend(config: CompareConfig) {
+    this._config = {
+      comparerFactories: [...this._config.comparerFactories, ...config.comparerFactories],
+      rules: { ...this._config.rules, ...config.rules },
+    };
+  }
+
+  /**
+   * Adds a comparison outcome rule to the configuration
+   *
+   * @param ruleKey - A key that is referenced from one of the comparer factories
+   * @param result - A version diff outcome based on this rule
+   */
+  public addRule(ruleKey: string, result: CompareResult) {
+    this._config.rules[ruleKey] = result;
+  }
+
+  /**
+   * Removes a comparison outcome rule from the configuration
+   *
+   * @param ruleKey - A key that is referenced from one of the comparer factories
+   * @throws {ReferenceError}
+   * Thrown if the `ruleKey` does not exist in the configuration
+   */
+  public removeRule(ruleKey: string) {
+    if (!this._config.rules[ruleKey]) throw new ReferenceError(`ruleKey '${ruleKey}' does not exist`);
+
+    delete this._config.rules[ruleKey];
+  }
+
+  /**
+   * Add a {@link ComparerFactory} to the configuration.
+   *
+   * @param f - A {@link ComparerFactory} that should reference the rules in the configuration
+   */
+  public addCompareFactory(f: ComparerFactory) {
+    this._config.comparerFactories = [...this._config.comparerFactories, f];
+  }
+}
