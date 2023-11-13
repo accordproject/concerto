b0a286db83b60a279a93b426c04f6705a1017a97
Muskan Bararia <muskan.b@docusign.com>
2023-11-13 18:52:01 +0530

    fix: removed two loops in parsing and better breakdown
    Signed-off-by: Muskan Bararia <muskan.b@docusign.com>

--- packages/concerto-core/lib/decoratorutil.js
+++ packages/concerto-core/lib/decoratorutil.js
@@ -25,95 +25,66 @@ const ModelUtil = require('./modelutil');
 class DecoratorUtil {
     /**
      * Parses the dict data into an array of decorator jsons
-     * @param {Object} jsonData - the model ast for a particular namespace
-     * @param {String} namespace - the namespace for which parsing is to be done
+     * @param {Array<Object>} dcsObjects - the array of collected dcs objects
+     * @param {Object} dcs - the current dcs json to be parsed
      * @param {String} DCS_VERSION - the version string
-     * @returns {Array<Object>} - the parsed decorator command set array
+     * @param {Object} target - target object for the command
+     * @returns {Array<Object>} - the array of collected dcs objects with the current dcs
      * @private
      */
-    static parseNonVocabularyDecorators(jsonData, namespace, DCS_VERSION){
-        const patternToDetermineVocab = /^Term_/i;
-        const dcsObjects = [];
-        jsonData.forEach(obj =>{
-            const decos = JSON.parse(obj.dcs);
-            const target = {
-                '$class': `org.accordproject.decoratorcommands@${DCS_VERSION}.CommandTarget`,
-                'namespace':namespace
-            };
-            if (obj.declaration && obj.declaration!==''){
-                target.declaration = obj.declaration;
-            }
-            if (obj.property && obj.property!==''){
-                target.property = obj.property;
-            }
-            if (obj.mapElement && obj.mapElement!==''){
-                target.mapElement = obj.mapElement;
-            }
-            decos.forEach(dcs =>{
-                if (dcs.name !== 'Term' && !patternToDetermineVocab.test(dcs.name)){
-                    const decotatorObj = {
-                        '$class': 'concerto.metamodel@1.0.0.Decorator',
-                        'name': dcs.name,
-                    };
-                    if (dcs.arguments){
-                        const args = dcs.arguments.map((arg)=>{
-                            return {
-                                '$class':arg.$class,
-                                'value':arg.value
-                            };
-                        });
-                        decotatorObj.arguments = args;
-                    }
-                    let dcsObject = {
-                        '$class': `org.accordproject.decoratorcommands@${DCS_VERSION}.Command`,
-                        'type': 'UPSERT',
-                        'target': target,
-                        'decorator': decotatorObj,
-                    };
-                    dcsObjects.push(dcsObject);
-                }
+    static parseNonVocabularyDecorators(dcsObjects, dcs, DCS_VERSION, target){
+        const decotatorObj = {
+            '$class': 'concerto.metamodel@1.0.0.Decorator',
+            'name': dcs.name,
+        };
+        if (dcs.arguments){
+            const args = dcs.arguments.map((arg)=>{
+                return {
+                    '$class':arg.$class,
+                    'value':arg.value
+                };
             });
-        });
+            decotatorObj.arguments = args;
+        }
+        let dcsObject = {
+            '$class': `org.accordproject.decoratorcommands@${DCS_VERSION}.Command`,
+            'type': 'UPSERT',
+            'target': target,
+            'decorator': decotatorObj,
+        };
+        dcsObjects.push(dcsObject);
         return dcsObjects;
     }
     /**
-     * @param {Object} jsonData - the model ast for a particular namespace
-     * @returns {Object} - the parsed vocab set object
+     * @param {Object} dictVoc - the collection of collected vocabularies
+     * @param {Object} decl - the declaration object
+     * @param {Object} dcs - the current dcs json to be parsed
+     * @returns {Object} - the collection of collected vocabularies with current dcs
      * @private
      */
-    static parseVocabularies(jsonData){
-        let dictVoc = {};
-        const patternToDetermineVocab = /^Term_/i;
-        jsonData.forEach(obj =>{
-            const decos = JSON.parse(obj.dcs);
-            decos.forEach(dcs =>{
-                if (dcs.name === 'Term' || patternToDetermineVocab.test(dcs.name)){
-                    dictVoc[obj.declaration] = dictVoc[obj.declaration] || { propertyVocabs: {} };
-                    if (obj.property !== ''){
-                        if (!dictVoc[obj.declaration].propertyVocabs[obj.property]){
-                            dictVoc[obj.declaration].propertyVocabs[obj.property] = {};
-                        }
-                        if (dcs.name === 'Term'){
-                            dictVoc[obj.declaration].propertyVocabs[obj.property].term = dcs.arguments[0].value;
-                        }
-                        else {
-                            const extensionKey = dcs.name.split('Term_')[1];
-                            dictVoc[obj.declaration].propertyVocabs[obj.property][extensionKey] = dcs.arguments[0].value;
-                        }
-                    }
-                    else {
-                        if (dcs.name === 'Term'){
-                            dictVoc[obj.declaration].term = dcs.arguments[0].value;
-                        }
-                        else {
-                            const extensionKey = dcs.name.split('Term_')[1];
-                            dictVoc[obj.declaration][extensionKey] = dcs.arguments[0].value;
-                        }
-                    }
-                }
-            });
-
-        });
+    static parseVocabularies(dictVoc, decl, dcs){
+        dictVoc[decl.declaration] = dictVoc[decl.declaration] || { propertyVocabs: {} };
+        if (decl.property !== ''){
+            if (!dictVoc[decl.declaration].propertyVocabs[decl.property]){
+                dictVoc[decl.declaration].propertyVocabs[decl.property] = {};
+            }
+            if (dcs.name === 'Term'){
+                dictVoc[decl.declaration].propertyVocabs[decl.property].term = dcs.arguments[0].value;
+            }
+            else {
+                const extensionKey = dcs.name.split('Term_')[1];
+                dictVoc[decl.declaration].propertyVocabs[decl.property][extensionKey] = dcs.arguments[0].value;
+            }
+        }
+        else {
+            if (dcs.name === 'Term'){
+                dictVoc[decl.declaration].term = dcs.arguments[0].value;
+            }
+            else {
+                const extensionKey = dcs.name.split('Term_')[1];
+                dictVoc[decl.declaration][extensionKey] = dcs.arguments[0].value;
+            }
+        }
         return dictVoc;
     }
     /**
@@ -144,10 +115,8 @@ class DecoratorUtil {
         }
         return dictionary;
     }
-
     /**
-    * Adds a key-value pair to a dictionary (object) if the key exists,
-    * or creates a new key with the provided value.
+    * parses the extracted decorators and generates arrays of decorator command set and vocabularies
     *
     * @param {Object} extractionDictionary - extracted decorators and vocabularies
     * @param {String} DCS_VERSION - dcs_version
@@ -163,8 +132,33 @@ class DecoratorUtil {
             const nameOfDcs = name;
             const versionOfDcs = version;
             const jsonData = extractionDictionary[namespace];
-            const dcsObjects = this.parseNonVocabularyDecorators(jsonData, namespace, DCS_VERSION);
-            const vocabObject = this.parseVocabularies(jsonData);
+            const patternToDetermineVocab = /^Term_/i;
+            let dcsObjects = [];
+            let vocabObject = {};
+            jsonData.forEach(obj =>{
+                const decos = JSON.parse(obj.dcs);
+                const target = {
+                    '$class': `org.accordproject.decoratorcommands@${DCS_VERSION}.CommandTarget`,
+                    'namespace':namespace
+                };
+                if (obj.declaration && obj.declaration!==''){
+                    target.declaration = obj.declaration;
+                }
+                if (obj.property && obj.property!==''){
+                    target.property = obj.property;
+                }
+                if (obj.mapElement && obj.mapElement!==''){
+                    target.mapElement = obj.mapElement;
+                }
+                decos.forEach(dcs =>{
+                    if (dcs.name !== 'Term' && !patternToDetermineVocab.test(dcs.name)){
+                        dcsObjects = this.parseNonVocabularyDecorators(dcsObjects, dcs, DCS_VERSION, target);
+                    }
+                    else{
+                        vocabObject = this.parseVocabularies(vocabObject, obj, dcs);
+                    }
+                });
+            });
             if (dcsObjects?.length > 0){
                 const dcmsForNamespace = {
                     '$class': `org.accordproject.decoratorcommands@${DCS_VERSION}.DecoratorCommandSet`,
@@ -211,6 +205,142 @@ class DecoratorUtil {
     }
 
     /**
+    * Process the properties to extract the decorators.
+    *
+    * @param {Object} sourceProperties - The source AST of the property
+    * @param {string} declarationName - The name of source declaration
+    * @param {Object} extractionDictionary - the collection of extracted decorators
+    * @param {boolean} removeDecoratorsFromModel - flag to decide whether to remove decorators or not
+    * @param {string} namespace - namespace of the model
+    * @returns {Object} - constructed DCS Dict and processed declarations ast
+    * @private
+    */
+    static processProperties(sourceProperties, declarationName, extractionDictionary, removeDecoratorsFromModel, namespace){
+        const processedProperties=sourceProperties.map(property => {
+            if (property.decorators){
+                const constructOptions = {
+                    declaration: declarationName,
+                    property: property.name
+                };
+                extractionDictionary = DecoratorUtil.constructDCSDictionary(extractionDictionary, namespace, property.decorators, constructOptions );
+            }
+            if (removeDecoratorsFromModel){
+                property.decorators = undefined;
+            }
+            return property;
+        });
+        return {
+            processedProperties,
+            extractionDictionary
+        };
+    }
+    /**
+    * Process the map declarations to extract the decorators.
+    *
+    * @param {Object} declaration - The source AST of the model
+    * @param {Object} extractionDictionary - the collection of extracted decorators
+    * @param {boolean} removeDecoratorsFromModel - flag to decide whether to remove decorators or not
+    * @param {string} namespace - namespace of the model
+    * @returns {Object} - constructed DCS Dict and processed declarations ast
+    * @private
+    */
+    static processMapDeclaration(declaration, extractionDictionary, removeDecoratorsFromModel, namespace){
+        if (declaration.key){
+            if (declaration.key.decorators){
+                const constructOptions = {
+                    declaration: declaration.name,
+                    mapElement: 'KEY'
+                };
+                extractionDictionary = DecoratorUtil.constructDCSDictionary(extractionDictionary, namespace, declaration.key.decorators, constructOptions);
+                if (removeDecoratorsFromModel){
+                    declaration.key.decorators = undefined;
+                }
+            }
+        }
+        if (declaration.value){
+            if (declaration.value.decorators){
+                const constructOptions = {
+                    declaration: declaration.name,
+                    mapElement: 'VALUE'
+                };
+                extractionDictionary = DecoratorUtil.constructDCSDictionary(extractionDictionary, namespace, declaration.value.decorators, constructOptions);
+                if (removeDecoratorsFromModel){
+                    declaration.value.decorators = undefined;
+                }
+            }
+        }
+        return {
+            declaration,
+            extractionDictionary
+        };
+    }
+    /**
+    * Process the declarations to extract the decorators.
+    *
+    * @param {Object} sourceDecl - The source AST of the model
+    * @param {Object} extractionDictionary - the collection of extracted decorators
+    * @param {boolean} removeDecoratorsFromModel - flag to decide whether to remove decorators or not
+    * @param {string} namespace - namespace of the model
+    * @returns {Object} - constructed DCS Dict and processed declarations ast
+    * @private
+    */
+    static processDeclarations(sourceDecl, extractionDictionary, removeDecoratorsFromModel, namespace){
+        const processedDecl = sourceDecl.map(decl => {
+            if (decl.decorators) {
+                const constructOptions = {
+                    declaration: decl.name,
+                };
+                extractionDictionary = DecoratorUtil.constructDCSDictionary(extractionDictionary, namespace, decl.decorators, constructOptions);
+            }
+            if (removeDecoratorsFromModel){
+                decl.decorators = undefined;
+            }
+            if (decl.$class.endsWith('.MapDeclaration')) {
+                const processedMapDeclData = this.processMapDeclaration(decl, extractionDictionary, removeDecoratorsFromModel, namespace);
+                decl = processedMapDeclData.declaration;
+                extractionDictionary=processedMapDeclData.extractionDictionary;
+            }
+            if (decl.properties) {
+                const processedPropertiesData = this.processProperties(decl.properties, decl.name, extractionDictionary, removeDecoratorsFromModel, namespace);
+                decl.properties = processedPropertiesData.processedProperties;
+                extractionDictionary = processedPropertiesData.extractionDictionary;
+            }
+            return decl;
+        });
+        return {
+            processedDecl,
+            extractionDictionary
+        };
+    }
+    /**
+    * Process the models to extract the decorators.
+    *
+    * @param {Object} sourceAst - The source AST of the model
+    * @param {boolean} removeDecoratorsFromModel - flag to decide whether to remove decorators or not
+    * @returns {Object} - constructed DCS Dict and processed models ast
+    * @private
+    */
+    static processModels(sourceAst, removeDecoratorsFromModel){
+        let extractionDictionary = {};
+        const processedModels = sourceAst.models.map(model =>{
+            if ((model?.decorators.length > 0)){
+                extractionDictionary = DecoratorUtil.constructDCSDictionary(extractionDictionary, model.namespace, model.decorators, {});
+                if (removeDecoratorsFromModel){
+                    model.decorators = undefined;
+                }
+            }
+            const processedDeclData = this.processDeclarations(model.declarations, extractionDictionary, removeDecoratorsFromModel, model.namespace);
+            model.declarations = processedDeclData.processedDecl;
+            extractionDictionary = processedDeclData.extractionDictionary;
+            return model;
+        });
+        return {
+            processedModels,
+            extractionDictionary
+        };
+    }
+
+    /**
     * Collects the decorators and vocabularies and updated the modelManager depending
     * on the options passed.
     *
@@ -222,75 +352,15 @@ class DecoratorUtil {
     * @private
     */
     static collectExtractedDecoratorsAndProcessedModels(modelManager, removeDecoratorsFromModel, locale, DCS_VERSION) {
-        let extractionDictionary = {};
         const sourceAst = modelManager.getAst(true);
-        const processedModels = sourceAst.models.map(model =>{
-            if ((model?.decorators.length > 0)){
-                extractionDictionary = DecoratorUtil.constructDCSDictionary(extractionDictionary, model.namespace, model.decorators, {});
-                if (removeDecoratorsFromModel){
-                    model.decorators = undefined;
-                }
-            }
-            const processedDecl = model.declarations.map(decl => {
-                if (decl.decorators) {
-                    const constructOptions = {
-                        declaration: decl.name,
-                    };
-                    extractionDictionary = DecoratorUtil.constructDCSDictionary(extractionDictionary, model.namespace, decl.decorators, constructOptions);
-                }
-                if (removeDecoratorsFromModel){
-                    decl.decorators = undefined;
-                }
-                if (decl.$class.endsWith('.MapDeclaration')) {
-                    if (decl.key){
-                        if (decl.key.decorators){
-                            const constructOptions = {
-                                declaration: decl.name,
-                                mapElement: 'KEY'
-                            };
-                            extractionDictionary = DecoratorUtil.constructDCSDictionary(extractionDictionary, model.namespace, decl.key.decorators, constructOptions);
-                            decl.key.decorators = undefined;
-                        }
-                    }
-                    if (decl.value){
-                        if (decl.value.decorators){
-                            const constructOptions = {
-                                declaration: decl.name,
-                                mapElement: 'VALUE'
-                            };
-                            extractionDictionary = DecoratorUtil.constructDCSDictionary(extractionDictionary, model.namespace, decl.value.decorators, constructOptions);
-                            decl.value.decorators = undefined;
-                        }
-                    }
-                }
-                if (decl.properties) {
-                    const processedProperties = decl.properties.map(property => {
-                        if (property.decorators){
-                            const constructOptions = {
-                                declaration: decl.name,
-                                property: property.name
-                            };
-                            extractionDictionary = DecoratorUtil.constructDCSDictionary(extractionDictionary, model.namespace, property.decorators, constructOptions );
-                        }
-                        if (removeDecoratorsFromModel){
-                            property.decorators = undefined;
-                        }
-                        return property;
-                    });
-                    decl.properties = processedProperties;
-                }
-                return decl;
-            });
-            model.declarations = processedDecl;
-            return model;
-        });
+        const processedData = this.processModels(sourceAst,removeDecoratorsFromModel);
         const processedAST={
             ...sourceAst,
-            models: processedModels,
+            models: processedData.processedModels,
         };
         const updatedModelManager = new ModelManager();
         updatedModelManager.fromAst(processedAST);
-        const extractedDecosAndVocabs = this.parseDecosAndVocabs(extractionDictionary, DCS_VERSION, locale);
+        const extractedDecosAndVocabs = this.parseDecosAndVocabs(processedData.extractionDictionary, DCS_VERSION, locale);
         return {
             updatedModelManager,
             decoratorCommandSet: extractedDecosAndVocabs.decoratorCommandSet,
