0369eb1eab475ecc2d7d289cc6201aeeb8f9ebdd
fuyalasmit <asmitphuyal4040@gmail.com>
2025-07-02 14:50:32 +0545

    initial structure of test file
    Signed-off-by: fuyalasmit <asmitphuyal4040@gmail.com>

---
+++ packages/concerto-core/test/dcsconverter.js
@@ -0,0 +1,66 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+describe('DCS Converter', ()=>{
+
+    describe('#jsonToYaml', function(){
+
+        beforeEach(()=>{
+
+        });
+
+        it('should extract decoratorCommandsVersion from $class', function(){
+
+        });
+        it('should remove all $class fields in output', function(){
+
+        });
+        it('should convert type to action key in output', function(){
+
+        });
+        it('should preserve all target fields', function(){
+
+        });
+        it('should handle empty arguments as null (empty value)', function(){
+
+        });
+        it('should extract type and value for all arguments type (String, Number, Boolean)', function(){
+
+        });
+        it('should convert DecoratorTypeReference properly', function(){
+
+        });
+        it('should handle includes and decoratorNamespace fields properly', function(){
+
+        });
+        it('should handle multiple commands in the input', function(){
+
+        });
+        it('should not modify the original JSON input', function(){
+
+        });
+        it('should output a valid YAML string', function(){
+
+        });
+        it('should throw error for invalid DCS JSON input', function(){
+
+        });
+
+    });
+
+});
+
+
