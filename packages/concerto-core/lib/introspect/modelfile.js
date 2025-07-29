fef9438946595f7d587f19174c4c228968eed5fc
Anshukumar123975 <anshupat2020@gmail.com>
2025-07-29 19:28:21 +0530

    Used singlequote
    Signed-off-by: Anshukumar123975 <anshupat2020@gmail.com>

--- packages/concerto-core/lib/introspect/modelfile.js
+++ packages/concerto-core/lib/introspect/modelfile.js
@@ -282,7 +282,7 @@ class ModelFile extends Decorated {
                 // Check if the imported type has the same short name as the local declaration
                 if (importShortName === localName) {
                     throw new IllegalModelException(
-                        `already defined in an imported model`,
+                        'already defined in an imported model',
                         this
                     );
                 }
