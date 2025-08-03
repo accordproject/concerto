d363a85dc3e317aa959b16a12da18e9475f30059
Anshukumar123975 <anshupat2020@gmail.com>
2025-08-03 19:24:52 +0530

    Comment the changes
    Signed-off-by: Anshukumar123975 <anshupat2020@gmail.com>

--- packages/concerto-core/lib/introspect/modelfile.js
+++ packages/concerto-core/lib/introspect/modelfile.js
@@ -225,7 +225,7 @@ class ModelFile extends Decorated {
 
         // Validate all of the imports to check that they reference
         // namespaces or types that actually exist.
-        const seenImportNamespace = new Set();
+        // const seenImportNamespace = new Set();
         this.getImports().forEach((importFqn) => {
             const importNamespace = ModelUtil.getNamespace(importFqn);
             const importShortName = ModelUtil.getShortName(importFqn);
@@ -242,14 +242,14 @@ class ModelFile extends Decorated {
             const existingNamespaceVersion = importsMap.get(name);
             // undefined means we haven't seen this namespace before,
             // null means we have seen it before but it didn't have a version
-            const unseenNamespace = existingNamespaceVersion === undefined;
-            if (seenImportNamespace.has(importNamespace)) {
-                throw new IllegalModelException(
-                    'Import namespace is already defined',
-                    this
-                );
-            }
-            seenImportNamespace.add(importNamespace);
+            // const unseenNamespace = existingNamespaceVersion === undefined;
+            // if (seenImportNamespace.has(importNamespace)) {
+            //     throw new IllegalModelException(
+            //         'Import namespace is already defined',
+            //         this
+            //     );
+            // }
+            // seenImportNamespace.add(importNamespace);
             // This check is needed because we automatically add both versioned and unversioned versions of
             // the root namespace for backwards compatibillity unless we're running in strict mode
             const isGlobalModel = name === 'concerto';
