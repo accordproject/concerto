aa0884de94580e014d242a3a74fa307c8ea6bcbd
Ahmed Gaper <223t.4hh@gmail.com>
2025-08-11 22:54:30 +0300

    feat(linter): Add string length, PascalCase decorator rules
    Signed-off-by: Ahmed Gaper <223t.4hh@gmail.com>

---
+++ packages/concerto-linter/default-ruleset/src/pascal-case-decorators.ts
@@ -0,0 +1,34 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { casing } from '@stoplight/spectral-functions';
+
+/**
+ * Rule: Pascal Case Decorators
+ * ------------------------------
+ * Ensures that decorator names follow PascalCase naming convention (e.g., 'MyDecorator').
+ * This promotes consistency and readability across model decorators.
+ */
+export default {
+    description: 'Decorators names should be PascalCase.',
+    given: '$.models[*].declarations[*].decorators[*].name',
+    message: 'Decorator \'{{value}}\' should be PascalCase (e.g. \'MyDecorator\')',
+    severity: 0, // 0 = error, 1 = warning, 2 = info, 3 = hint
+    then: {
+        function: casing,
+        functionOptions: {
+            type: 'pascal',
+        },
+    },
+};
