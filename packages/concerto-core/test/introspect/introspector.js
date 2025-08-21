ad32d1699d4334221c497bd7cfbf41501df0ffd5
Anshukumar123975 <anshupat2020@gmail.com>
2025-08-21 22:33:52 +0530

    Some testing changes
    Signed-off-by: Anshukumar123975 <anshupat2020@gmail.com>

--- packages/concerto-core/test/introspect/introspector.js
+++ packages/concerto-core/test/introspect/introspector.js
@@ -103,7 +103,7 @@ describe('Introspector', () => {
             let modelFile1 = ParserUtil.newModelFile(modelManager, model1);
             modelManager.addModelFile(modelFile1);
             let modelFile2 = ParserUtil.newModelFile(modelManager, model2);
-            modelManager.validateModelFiles();  // Added after discussion
+            // modelManager.validateModelFiles();  // Added after discussion
             const introspector = new Introspector(modelManager);
             modelFile2.resolveImport('m').should.equal('org.example.ext.MyAsset2');
             introspector.getClassDeclaration('org.example.ext.MyAsset2').should.not.be.null;
