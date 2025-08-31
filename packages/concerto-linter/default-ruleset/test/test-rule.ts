4a5a20ba5587fd9c12a63bdec39aec75676dbd37
Ahmed Gaper <223t.4hh@gmail.com>
2025-08-31 23:39:31 +0300

    refactor(linter): Update workflow for obtaining parsed AST of .cto model
    Signed-off-by: Ahmed Gaper <223t.4hh@gmail.com>

--- packages/concerto-linter/default-ruleset/test/test-rule.ts
+++ packages/concerto-linter/default-ruleset/test/test-rule.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs/promises';
 import * as path from 'path';
-import { ModelManager } from '@accordproject/concerto-core';
+import { Parser } from '@accordproject/concerto-cto';
 import { Json as JsonParsers } from '@stoplight/spectral-parsers';
 import { Spectral, Document, IRuleResult, RulesetDefinition } from '@stoplight/spectral-core';
 
@@ -8,6 +8,7 @@ import { Spectral, Document, IRuleResult, RulesetDefinition } from '@stoplight/s
  * Tests the Concerto linter's default ruleset.
  * @param {RulesetDefinition} [ruleset] - Optional ruleset to apply for testing.
  * @param {string} modelFile - The model (CTO string) to be linted.
+ Note: No external dependency resolution is performed.
  * @returns {Promise<IRuleResult[]>} A promise that resolves to an array of linting results.
  * @throws {Error} If a critical processing failure occurs during linting.
  */
@@ -16,9 +17,7 @@ export async function testRules(ruleset: RulesetDefinition, modelFile: string): 
         const filePath = path.resolve(__dirname, './fixtures/', modelFile);
         const model = await fs.readFile(filePath, 'utf-8');
 
-        const manager = new ModelManager();
-        manager.addCTOModel(model);
-        const jsonAST = JSON.stringify(manager.getAst());
+        const jsonAST = JSON.stringify(Parser.parseModels([model]));
 
         const spectral = new Spectral();
         spectral.setRuleset(ruleset);
