b481f75f53b96db4261052a4264ee148d7b89f14
Aaditya Vardhan Singh <103750395+rockaxorb13@users.noreply.github.com>
2026-02-03 22:46:58 +0530

    Fix/windows build v4 (#1120)
    * fix(build): use cross-env and cpx for windows compatibility
    
    Signed-off-by: Aadityavardhan Singh <singhrashmi018@gmail.com>
    
    * build: fix windows linting and restore pretest checks
    
    Signed-off-by: Aadityavardhan Singh <singhrashmi018@gmail.com>
    
    ---------
    
    Signed-off-by: Aadityavardhan Singh <singhrashmi018@gmail.com>

--- packages/concertino/src/spec/concerto@1.0.0.ts
+++ packages/concertino/src/spec/concerto@1.0.0.ts
@@ -5,40 +5,40 @@
 
 // Warning: Beware of circular dependencies when modifying these imports
 import type {
-	IPosition,
-	IRange,
-	ITypeIdentifier,
-	IDecoratorLiteral,
-	IDecorator,
-	IIdentified,
-	IDeclaration,
-	IMapKeyType,
-	IMapValueType,
-	IEnumProperty,
-	IProperty,
-	IStringRegexValidator,
-	IStringLengthValidator,
-	IDoubleDomainValidator,
-	IIntegerDomainValidator,
-	ILongDomainValidator,
-	IAliasedType,
-	IImport,
-	IModel,
-	IModels
+    IPosition,
+    IRange,
+    ITypeIdentifier,
+    IDecoratorLiteral,
+    IDecorator,
+    IIdentified,
+    IDeclaration,
+    IMapKeyType,
+    IMapValueType,
+    IEnumProperty,
+    IProperty,
+    IStringRegexValidator,
+    IStringLengthValidator,
+    IDoubleDomainValidator,
+    IIntegerDomainValidator,
+    ILongDomainValidator,
+    IAliasedType,
+    IImport,
+    IModel,
+    IModels
 } from './concerto.metamodel@1.0.0';
 import type {
-	IConcertino,
-	IMetadata,
-	IModelMetadata,
-	IConcertinoDeclaration,
-	Prototype,
-	IConcertinoEnumValue,
-	IMapKey,
-	IMapValue,
-	IConcertinoProperty,
-	IVocabulary,
-	IDecoratorValues,
-	IDecoratorValue
+    IConcertino,
+    IMetadata,
+    IModelMetadata,
+    IConcertinoDeclaration,
+    Prototype,
+    IConcertinoEnumValue,
+    IMapKey,
+    IMapValue,
+    IConcertinoProperty,
+    IVocabulary,
+    IDecoratorValues,
+    IDecoratorValue
 } from './concertino.metamodel@4.0.0-alpha.2';
 
 // interfaces
@@ -46,36 +46,36 @@ export interface IConcept {
    $class?: string;
 }
 
-export type ConceptUnion = IPosition | 
-IRange | 
-ITypeIdentifier | 
-IDecoratorLiteral | 
-IDecorator | 
-IIdentified | 
-IDeclaration | 
-IMapKeyType | 
-IMapValueType | 
-IEnumProperty | 
-IProperty | 
-IStringRegexValidator | 
-IStringLengthValidator | 
-IDoubleDomainValidator | 
-IIntegerDomainValidator | 
-ILongDomainValidator | 
-IAliasedType | 
-IImport | 
-IModel | 
-IModels | 
-IConcertino | 
-IMetadata | 
-IModelMetadata | 
-IConcertinoDeclaration | 
-IConcertinoEnumValue | 
-IMapKey | 
-IMapValue | 
-IConcertinoProperty | 
-IVocabulary | 
-IDecoratorValues | 
+export type ConceptUnion = IPosition |
+IRange |
+ITypeIdentifier |
+IDecoratorLiteral |
+IDecorator |
+IIdentified |
+IDeclaration |
+IMapKeyType |
+IMapValueType |
+IEnumProperty |
+IProperty |
+IStringRegexValidator |
+IStringLengthValidator |
+IDoubleDomainValidator |
+IIntegerDomainValidator |
+ILongDomainValidator |
+IAliasedType |
+IImport |
+IModel |
+IModels |
+IConcertino |
+IMetadata |
+IModelMetadata |
+IConcertinoDeclaration |
+IConcertinoEnumValue |
+IMapKey |
+IMapValue |
+IConcertinoProperty |
+IVocabulary |
+IDecoratorValues |
 IDecoratorValue;
 
 export interface IAsset extends IConcept {
