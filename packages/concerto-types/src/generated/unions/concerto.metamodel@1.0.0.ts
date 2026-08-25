9d93a7b6f982ce741a43430c215db381d5ff22c1
muhammed-abdulkadir <muhammed.abdulkadir@docusign.com>
2026-08-25 14:21:54 +0100

    chore(types): update generated types
    Signed-off-by: muhammed-abdulkadir <muhammed.abdulkadir@docusign.com>

--- packages/concerto-types/src/generated/unions/concerto.metamodel@1.0.0.ts
+++ packages/concerto-types/src/generated/unions/concerto.metamodel@1.0.0.ts
@@ -195,6 +195,7 @@ export interface IProperty extends IConcept {
    name: string;
    isArray: boolean;
    isOptional: boolean;
+   sizeValidator?: ICollectionSizeValidator;
    decorators?: IDecorator[];
    location?: IRange;
 }
@@ -207,6 +208,11 @@ IStringProperty |
 IDoubleProperty | 
 IIntegerProperty | 
 ILongProperty;
+
+export interface ICollectionSizeValidator extends IConcept {
+   minSize?: number;
+   maxSize?: number;
+}
 
 export interface IRelationshipProperty extends IProperty {
    type: ITypeIdentifier;
